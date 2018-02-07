1ed9df0c845f811589f14a32d3e946f82e27ad9f
unknown <you@example.com>
2018-02-06 18:10:24 -0800

    there is a bug somewhere in my code. I'm tired

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+    // componentDidMount will automatically be called by react immediatelyb after this component is called by the DOM
+    // ^will not be called if there is a typo i.e. the function MUST be spelled exactly as it is (including case-sensitivity)
+
     render() {
         return (
             <div>
@@ -10,4 +18,6 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
+// We can simply type { fetchPosts } rather than { fetchPosts: fetchPosts } because the key and value are identical
+// i.e. we can do this in ES6

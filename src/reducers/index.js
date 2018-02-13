c93475ac1d173c52cae8338706bf8a39064e3df2
unknown <you@example.com>
2018-02-12 18:06:47 -0800

    added formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+// we refer to the form as 'formReducer' rather than simply as 'reducer.'
+// we do this because apparently 'reducer' is too generic of a keyword
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;

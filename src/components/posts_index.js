72ac94c9e8fb20484a4c9d4e7f04e8574bce37f6
unknown <you@example.com>
2018-02-12 17:44:38 -0800

    added JSON data to something

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -9,15 +10,35 @@ class PostsIndex extends Component {
     // componentDidMount will automatically be called by react immediatelyb after this component is called by the DOM
     // ^will not be called if there is a typo i.e. the function MUST be spelled exactly as it is (including case-sensitivity)
 
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className='list-group-item'>
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
+        console.log(this.props.posts);
         return (
             <div>
-                Posts index 
+                <h3>Posts</h3>
+                <ul className='list-group'>
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+    return {
+        posts: state.posts
+    };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
 // We can simply type { fetchPosts } rather than { fetchPosts: fetchPosts } because the key and value are identical
 // i.e. we can do this in ES6

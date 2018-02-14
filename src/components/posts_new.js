5b6ec28033def6ae5e2f21e031a88e6c741f2d6a
unknown <you@example.com>
2018-02-13 18:06:53 -0800

    validations only trigger display after user touches form/button

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta: { touched, error } } = field;
+        // the line above allows us to not have to write out 'meta.touched' and/or 'meta.error' in the line below (as well as any lines of code below that uses the same 'syntax' (? not sure if that's the best way to phrase that))
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className='form-group'>
+            <div className={className}>
             <label>{field.label}</label>
                 <input
                 className='form-control'
@@ -12,8 +17,11 @@ class PostsNew extends Component {
                     {...field.input}
                     // this syntax prevents us from needing to input multiple onChange functions
                 />
-                {field.meta.error}
+                <div className='text-help'>
+                    {touched ? error : ''}
+                </div>
             </div>
+            // note: 'touched' refers to the three form/field states (i.e. pristine, touched, and invalid)
         );
     }
 
@@ -43,6 +51,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type='submit' className='btn btn-primary'>Submit</button>
+                <Link to='/' className='btn btn-danger'>Cancel</Link>
             </form>
             // so field functions similarly to 'input' in plain HTML?
         );
@@ -57,7 +66,7 @@ function validate(values) {
         errors.title = 'Enter a title';
     }
     if (!values.categories) {
-        errors.title = 'Enter some categories';
+        errors.categories = 'Enter some categories';
     }
     if (!values.content) {
         errors.content = 'Enter some content';

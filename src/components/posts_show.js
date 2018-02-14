b87569a70b2ce2d6b6f61968e07d0e4b2bfea640
unknown <you@example.com>
2018-02-14 15:09:58 -0800

    there's a bug somewhere that's preventing stuff from displaying

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,13 +3,31 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params.id;
+        // the stuff above (to the right of the const declaration) is provided to us via react router
+        this.props.fetchPost();
+    }
+
     render() {
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                Posts Show
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
-    };
+    }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps({ posts }, ownProps) {
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);

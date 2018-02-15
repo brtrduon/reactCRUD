5da6210518607eff93831777df82e8cfc0c5bd79
unknown <you@example.com>
2018-02-14 17:34:45 -0800

    added delete functionality

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
+    case DELETE_POST:
+        return _.omit(state, action.payload);
+        // if the state object has a key of the post id, then it will be omitted and be returned with a new object that doesn't contain that id
+        // does not modify the existing state object, but it returns a new state object with that particular state id not present anymore
     case FETCH_POST:
         // const post = action.payload.data;
         // const newState = { ...state };
